081e8dbc1b0849301bada3d49ced226f5692df19
Luís Pinto <luispinto23@gmail.com>
2023-12-05 23:46:16 +0000

    feat: DELETE movie handler.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -90,6 +90,28 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+  DELETE FROM movies
+  WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsaffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsaffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
 
